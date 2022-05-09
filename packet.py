81975e315ea35aefd9e790c01166c88cbf204f63
Hass <seennd1@gmail.com>
2022-05-09 21:26:14 +0300

    almost done testing

---
+++ packet.py
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+
+
+class Packet:
+
+
+
+	def __init__( self, src_ip,des_ip,src_city,src_country,des_city,des_coutry,src_port,des_port,protocol, dns_name):
+
+
+		self.src_ip=src_ip
+		self.des_ip=des_ip
+		self.src_city=src_city
+		self.src_country=src_country
+		self.des_city=des_city
+		self.des_coutry=des_coutry
+		self.src_port=src_port
+		self.des_port=des_port
+		self.protocol=protocol
+		self.dns_name=dns_name
+
+
