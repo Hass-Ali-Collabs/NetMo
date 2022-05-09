150c98638957c953f4f1af568afab7ccc9723f11
Hass <seennd1@gmail.com>
2022-05-09 23:19:23 +0300

    finished filtering and turing the caps into an arry

--- buildind.py
+++ buildind.py
@@ -6,7 +6,7 @@ class Packet:
 
 
 
-	def __init__( self, src_ip,des_ip,src_city,src_country,des_city,des_coutry,src_port,des_port,protocol, dns_name):
+	def __init__( self, src_ip,des_ip,src_city,src_country,des_city,des_coutry,src_port,des_port,protocol, dns_name,prity):
 
 
 		self.src_ip=src_ip
@@ -19,6 +19,7 @@ class Packet:
 		self.des_port=des_port
 		self.protocol=protocol
 		self.dns_name=dns_name
+		self.prity=prity
 
 
 
@@ -33,41 +34,56 @@ l=[]
 
 def creat(pkt):
     try:
+
         protocol =  pkt.transport_layer
         src_addr = pkt.ip.src
         src_port = pkt[pkt.transport_layer].srcport
         dst_addr = pkt.ip.dst
         dst_port = pkt[pkt.transport_layer].dstport
-        p = Packet(src_addr,dst_addr,"x","x","x","x",src_port,dst_port,pkt.transport_layer,"")
+        p = Packet(src_addr,dst_addr,"x","x","x","x",src_port,dst_port,pkt.transport_layer,"",pkt.pretty_print)
         l.append(p)
     except AttributeError as e:
         #ignore packets that aren't TCP/UDP or IPv4
         pass
 
-
 IN=0
 
 def add_DNS(pkt):
+	global IN
+	if(IN==len(l)):
+		return none
+	
 	try:
 		if pkt.dns.qry_name:
-			#l[pkt.no].dns_name=pkt.dms.qry_name
+			l[IN].dns_name=pkt.dms.qry_name
 			print(pkt.dms.qry_name)
-			#IN+=1
 	except AttributeError as e:
-		#IN+=1
+		l[IN].dns_name=""
         #ignore packets that aren't DNS Request
-		#print(pkt.no)
 		pass
 	try:
 		if pkt.dns.resp_name:
-			#l[pkt.no].dns_name=pkt.dms.qry_name
+			l[IN].dns_name=pkt.dms.qry_name
 			print(pkt.dms.resp_name)
 
-			#IN+=1
 	except AttributeError as e:
-		#IN+=1
+		l[IN].dns_name=""
         #ignore packets that aren't DNS Response
 		pass
+	IN +=1
+	print(IN) 
 
 C.apply_on_packets(creat, timeout=100)
 C.apply_on_packets(add_DNS, timeout=100)
+print(l[0].src_ip)
+print(l[0].des_ip)
+print(l[0].src_city)
+print(l[0].src_country)
+print(l[0].des_city)
+print(l[0].des_coutry)
+print(l[0].src_port)
+print(l[0].des_port)
+print(l[0].protocol)
+print(l[0].dns_name)
+print(l[0].prity)
+
--- main.py
+++ main.py
@@ -7,8 +7,30 @@ exec(open("building.py").read())
 
 
 
-def IP_sorting():
-	pass
+def IP_sorting(ip):
+	global l
+	ipl=[]
+	for x in xrange(0,len(l)):
+		if l[x].src_ip == ip:
+			ipl.append(l[x])
+		else:
+			pass
+	return ipl
+
+
+def only_web(paks):
+	webl
+	for x in xrange(0,len(paks)):
+		if paks.dns_name != "":
+			webl.append(paks[x])
+
+	return webl
+
+		
+
+
+		
+	
 
 
 
