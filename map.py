8ea41b8d897e6ce8f9b5b6b1aa7a971a376d79bb
Ali Hamed <alihamed.cg@gmail.com>
2022-05-10 23:17:34 +0300

    create the map

--- map.py
+++ map.py
@@ -1,30 +1,27 @@
+import folium
 
+m = folium.Map(location=[52.37613757431285, 4.927801674596649], zoom_start=3)
+m.save('map.html')
 
-import requests
-from flask import Flask,render_template
+# folium.Marker([52.376111375557294, 4.927876776449689],
+# 	popup="<h1>hello</h1>",tooltip = "the location name",
+# 	icon=folium.Icon(icon = 'heart',icon_color='red')).add_to(m)
 
-URL = "https://geocode.search.hereapi.com/v1/geocode"
-location = input("input your location") #taking user input
-api_key = 'yyyy' # Acquire from developer.here.com
-PARAMS = {'apikey':api_key,'q':location} 
+# Import the pandas library
+import pandas as pd
 
-# sending get request and saving the response as response object 
-r = requests.get(url = URL, params = PARAMS) 
-data = r.json()
-#print(data)
+# Make a data frame with dots to show on the map
+data = pd.DataFrame({
+   'lon':[-58, 2, 145, 30.32, -4.03, -73.57, 36.82, -38.5],
+   'lat':[-34, 49, -38, 59.93, 5.33, 45.52, -1.29, -12.97],
+   'name':['Buenos Aires', 'Paris', 'melbourne', 'St Petersbourg', 'Abidjan', 'Montreal', 'Nairobi', 'Salvador'],
+   'value':[10, 12, 40, 70, 23, 43, 100, 43]
+}, dtype=str)
 
-#Acquiring the latitude and longitude from JSON 
-latitude = 33.888630
-#print(latitude)
-longitude = 35.495480
-#print(longitude)
-
-#Flask code 
-app = Flask(__name__)
-@app.route('/')
-
-def map_func():
-	return render_template('map.html',apikey=api_key,latitude=latitude,longitude=longitude)#map.html is my HTML file name
-
-if __name__ == '__main__':
-    app.run(debug = False)
+# add marker one by one on the map
+for i in range(0,len(data)):
+   folium.Marker(
+      location=[data.iloc[i]['lat'], data.iloc[i]['lon']],
+      popup=data.iloc[i]['name'],
+   ).add_to(m)
+   m.save('map.html')
